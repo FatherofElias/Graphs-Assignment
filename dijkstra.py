c0cbe64127a8d72992a426dab5c754c35dba64cc
FatherofElias <supra_3_8@hotmail.com>
2024-11-24 13:20:27 -0600

    update

--- dijkstra.py
+++ dijkstra.py
@@ -1,23 +1,29 @@
-# Task 2
+# Task 1
 
 class Graph:
     def __init__(self):
+        # Initializes an empty dictionary to store vertices and their edges
         self.vertices = {}
 
     def add_vertex(self, vertex):
+        # Adds a vertex to the graph if it doesn't already exist
         if vertex not in self.vertices:
             self.vertices[vertex] = {}
 
     def add_edge(self, source, destination, weight):
+        # Adds an edge between two vertices with a given weight
         if source in self.vertices and destination in self.vertices:
             self.vertices[source][destination] = weight
             self.vertices[destination][source] = weight  # For undirected graph
 
     def get_neighbors(self, vertex):
+        # Returns the neighbors of a given vertex along with the edge weights
         if vertex in self.vertices:
             return self.vertices[vertex]
         else:
             return {}
+
+# Task 2
 
 def dijkstra(graph, start_vertex):
     # Initialize distances and predecessors
